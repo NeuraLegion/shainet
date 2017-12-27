9a0f7bb41a8c2d5e91f8d615e2f8f823ec2c2599
Art <Artlinkov@gmail.com>
2017-12-27 11:12:03 +0200

    bug fixes

--- spec/network_spec.cr
+++ spec/network_spec.cr
@@ -57,7 +57,7 @@ describe SHAInet::Network do
     puts normalized
     iris.train(normalized.data, :sgdm, :mse, :sigmoid, 20000, 0.1)
     iris.run(normalized.normalized_inputs.first)
-    puts "Expected output is: [0,0,1]"
+    ((result.first < 0.3) && (result[1] < 0.3) && (result.last > 0.7)).should eq(true)
   end
 
   it "works on iris dataset with batch train with Rprop" do
