8f36270d1fcb81dca6e24ae375b462d59f3e7140
Dru Jensen <drujensen@gmail.com>
2018-01-06 08:01:26 -0800

    test to demonstrate different results

---
+++ spec/load_save_spec.cr
@@ -0,0 +1,39 @@
+require "./spec_helper"
+require "csv"
+
+# Extract train data
+system("cd #{__DIR__}/test_data && tar xvf tests.tar.xz")
+
+describe SHAInet::Network do
+  it "Saves XOR to a file" do
+    training_data = [
+      [[0, 0], [0]],
+      [[1, 0], [1]],
+      [[0, 1], [1]],
+      [[1, 1], [0]],
+    ]
+
+    xor = SHAInet::Network.new
+    xor.add_layer(:input, 2, :memory, SHAInet.sigmoid)
+    1.times { |x| xor.add_layer(:hidden, 3, :memory, SHAInet.sigmoid) }
+    xor.add_layer(:output, 1, :memory, SHAInet.sigmoid)
+    xor.fully_connect
+    # data, training_type, cost_function, activation_function, epochs, error_threshold (MSE %), log each steps
+    xor.train(training_data, :sgdm, :mse, epochs = 5000, threshold = 0.000001, log = 100)
+    xor.save_to_file("./xor.nn")
+    (xor.run([0, 0]).first < 0.1).should eq(true)
+    (xor.run([1, 0]).first > 0.9).should eq(true)
+    (xor.run([0, 1]).first > 0.9).should eq(true)
+    (xor.run([1, 1]).first < 0.1).should eq(true)
+  end
+
+  it "Loads XOR to a file" do
+    xor = SHAInet::Network.new
+    xor.load_from_file("./xor.nn")
+
+    (xor.run([0, 0]).first < 0.1).should eq(true)
+    (xor.run([1, 0]).first > 0.9).should eq(true)
+    (xor.run([0, 1]).first > 0.9).should eq(true)
+    (xor.run([1, 1]).first < 0.1).should eq(true)
+  end
+end
