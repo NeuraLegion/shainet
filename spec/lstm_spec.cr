0da887e266a77dbb6371602128689f2bf45a9a6e
Bar Hofesh <bar.hofesh@gmail.com>
2025-06-23 16:27:44 +0300

    Add sequence learning tests for RNN and LSTM

--- spec/lstm_spec.cr
+++ spec/lstm_spec.cr
@@ -16,4 +16,19 @@ describe SHAInet::LSTMLayer do
     outputs = net.run(seq)
     outputs.size.should eq(3)
   end
+
+  it "learns to predict the last value of a sequence" do
+    net = SHAInet::Network.new
+    net.add_layer(:input, 1, :memory, SHAInet.none)
+    net.add_layer(:lstm, 1)
+    net.add_layer(:output, 1, :memory, SHAInet.none)
+    net.fully_connect
+
+    seq = [[1.0], [2.0], [3.0]]
+    expected = 3.0
+    net.train([[seq, [expected]]], training_type: :sgdm,
+      epochs: 500, mini_batch_size: 1, log_each: 500)
+    result = net.run(seq).last.first
+    result.should be_close(expected, 0.1)
+  end
 end
