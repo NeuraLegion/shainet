d3ebc92705fcaff11c3627d9b28397a6635ec13a
Bar Hofesh <bar.hofesh@gmail.com>
2025-06-25 17:27:31 +0300

    added logs, format

--- src/shainet.cr
+++ src/shainet.cr
@@ -3,6 +3,9 @@ require "./shainet/**"
 require "apatite"
 
 module SHAInet
+  Log = ::Log.for(self)
   include Apatite
   alias GenNum = Float64 | Int32 | Int64 | Float32
+
+  ::Log.setup(:debug)
 end
