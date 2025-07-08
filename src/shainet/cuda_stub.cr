a14ce126db50096094432d338ebba7af7234f1a1
Bar Hofesh <bar.hofesh@gmail.com>
2025-07-08 13:16:43 +0300

    Add softmax cross entropy label CUDA kernel

--- src/shainet/cuda_stub.cr
+++ src/shainet/cuda_stub.cr
@@ -175,6 +175,10 @@ module SHAInet
       raise "CUDA kernels not available"
     end
 
+    def softmax_cross_entropy_label(*args) : Int32
+      raise "CUDA kernels not available"
+    end
+
     def dropout(*args) : Int32
       raise "CUDA kernels not available"
     end
